86f83d3806fb2b0068e17d6f9e4f71995fbd14c9
Augustin <48162609+AugustinSorel@users.noreply.github.com>
2022-09-11 19:11:09 +0100

    login page added (#3)

--- src/components/UserForm/index.tsx
+++ src/components/UserForm/index.tsx
@@ -1,23 +1,45 @@
 import Button from "@/components/Button";
 import Input from "@/components/Input";
-import { FormEvent } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import * as Styles from "./index.styled";
 
-const UserForm = () => {
+type Props = {
+  role: "sign-up" | "login";
+};
+
+const UserForm = ({ role }: Props) => {
+  const defaultUser = {
+    ...(role === "sign-up" && { name: "" }),
+    email: "",
+    password: "",
+  };
+
+  const [user, setUser] = useState(defaultUser);
+
   const submitHandler = (e: FormEvent) => {
     e.preventDefault();
   };
 
+  const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setUser((old) => ({ ...old, [e.target.name]: e.target.value }));
+  };
+
   return (
     <Styles.Form onSubmit={submitHandler}>
-      <Input
-        role="form"
-        labelText="name"
-        htmlFor="nameInput"
-        placeholder="Enter your name"
-        autoComplete="off"
-        errorText=""
-      />
+      {role !== "login" && (
+        <Input
+          role="form"
+          labelText="name"
+          htmlFor="nameInput"
+          placeholder="Enter your name"
+          autoComplete="off"
+          errorText=""
+          value={user.name}
+          name={"name"}
+          onChange={changeHandler}
+        />
+      )}
+
       <Input
         role="form"
         labelText="email"
@@ -25,7 +47,11 @@ const UserForm = () => {
         placeholder="Enter your email"
         autoComplete="new-password"
         errorText=""
+        value={user.email}
+        name="email"
+        onChange={changeHandler}
       />
+
       <Input
         role="form"
         labelText="password"
@@ -33,9 +59,16 @@ const UserForm = () => {
         htmlFor="passwordInput"
         placeholder="•••••••••••••"
         errorText=""
+        value={user.password}
+        name="password"
+        onChange={changeHandler}
       />
 
-      <Button role="callToAction" text="Sign up" type="submit" />
+      <Button
+        role="callToAction"
+        text={role === "login" ? "Login" : "Sign up"}
+        type="submit"
+      />
     </Styles.Form>
   );
 };

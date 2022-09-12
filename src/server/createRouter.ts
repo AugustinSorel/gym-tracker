8522aee67ba67f1d0d1da9907cb54ac787afe6d8
AugustinSorel <sorelaugustin@gmail.com>
2022-09-12 21:04:53 +0100

    router format error added

--- src/server/createRouter.ts
+++ src/server/createRouter.ts
@@ -1,5 +1,17 @@
 import { router } from "@trpc/server";
+import { ZodError } from "zod";
 
-const createRouter = router();
+const createRouter = router().formatError(({ shape, error }) => {
+  return {
+    ...shape,
+    data: {
+      ...shape.data,
+      zodError:
+        error.code === "BAD_REQUEST" && error.cause instanceof ZodError
+          ? error.cause.flatten()
+          : null,
+    },
+  };
+});
 
 export default createRouter;

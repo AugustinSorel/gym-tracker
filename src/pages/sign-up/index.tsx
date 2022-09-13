b583a98c40b175db051f0f9f4815f7ddc3fd2927
AugustinSorel <sorelaugustin@gmail.com>
2022-09-13 18:27:31 +0100

    sign up api called added

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,10 +1,56 @@
-import UserForm from "@/components/UserForm";
+import Button from "@/components/Button";
+import Input from "@/components/Input";
+import * as Styles from "@/components/UserForm/index.styled";
 import Head from "next/head";
-import { ReactElement } from "react";
+import { ChangeEvent, FormEvent, ReactElement, useState } from "react";
 import AuthLayout from "src/layouts/AuthLayout";
+import trpc from "src/utils/trpc";
 import { NextPageWithLayout } from "../_app";
 
+const defaultUser = {
+  name: "",
+  email: "",
+  password: "",
+};
+
 const SignUpPage: NextPageWithLayout = () => {
+  const [user, setUser] = useState(defaultUser);
+  const [errors, setErrors] = useState(defaultUser);
+
+  const { mutate } = trpc.useMutation(["user.create"], {
+    onSuccess: () => {
+      setUser(defaultUser);
+    },
+
+    onError: (error) => {
+      if (error.data?.zodError) {
+        const { name, email, password } = error.data.zodError.fieldErrors;
+        setErrors((old) => ({
+          name: (name || [])[0] || old.name,
+          email: (email || [])[0] || old.email,
+          password: (password || [])[0] || old.password,
+        }));
+      }
+
+      if (error.data?.code === "CONFLICT") {
+        setErrors((old) => ({ ...old, email: error.message }));
+      }
+    },
+
+    onMutate: () => {
+      setErrors(defaultUser);
+    },
+  });
+
+  const submitHandler = (e: FormEvent) => {
+    e.preventDefault();
+    mutate(user);
+  };
+
+  const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setUser((old) => ({ ...old, [e.target.name]: e.target.value }));
+  };
+
   return (
     <>
       <Head>
@@ -13,7 +59,45 @@ const SignUpPage: NextPageWithLayout = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <UserForm role="sign-up" />
+      <Styles.Form onSubmit={submitHandler}>
+        <Input
+          role="form"
+          labelText="name"
+          htmlFor="nameInput"
+          placeholder="Enter your name"
+          autoComplete="off"
+          errorText={errors.name}
+          value={user.name}
+          name={"name"}
+          onChange={changeHandler}
+        />
+
+        <Input
+          role="form"
+          labelText="email"
+          htmlFor="emailInput"
+          placeholder="Enter your email"
+          autoComplete="new-password"
+          errorText={errors.email}
+          value={user.email}
+          name="email"
+          onChange={changeHandler}
+        />
+
+        <Input
+          role="form"
+          labelText="password"
+          type="password"
+          htmlFor="passwordInput"
+          placeholder="•••••••••••••"
+          errorText={errors.password}
+          value={user.password}
+          name="password"
+          onChange={changeHandler}
+        />
+
+        <Button role="callToAction" text="Sign up" type="submit" />
+      </Styles.Form>
     </>
   );
 };

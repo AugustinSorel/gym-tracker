7a035e9a8321ad7f59ee155384a14e9d84b7fc36
AugustinSorel <sorelaugustin@gmail.com>
2022-09-13 18:27:39 +0100

    user create resolver added

--- src/server/routers/user.ts
+++ src/server/routers/user.ts
@@ -1,12 +1,29 @@
-import { router } from "@trpc/server";
+import { router, TRPCError } from "@trpc/server";
 import * as userSchemas from "@/schemas/userSchemas";
 import * as userServices from "../services/userServices";
+import { Prisma } from "@prisma/client";
+import * as bcrypt from "src/utils/bcrypts";
 
 const userRouter = router().mutation("create", {
   input: userSchemas.create,
   resolve: async ({ input }) => {
-    const user = await userServices.create({ data: input });
-    return user;
+    try {
+      return await userServices.create({
+        data: {
+          ...input,
+          password: await bcrypt.encryptString(input.password),
+        },
+      });
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === "P2002") {
+          throw new TRPCError({
+            code: "CONFLICT",
+            message: "email is already used",
+          });
+        }
+      }
+    }
   },
 });
 

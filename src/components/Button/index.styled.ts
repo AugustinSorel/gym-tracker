49f980a2b13c8e32260caf29b320ff9a9bfcf9f8
AugustinSorel <sorelaugustin@gmail.com>
2022-09-13 20:37:08 +0100

    loader added

--- src/components/Button/index.styled.ts
+++ src/components/Button/index.styled.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css, keyframes } from "styled-components";
 
 type Props = {
   type?: "submit" | "button" | "reset";
@@ -13,7 +13,8 @@ export const Button = styled.button.attrs(({ type }) => ({
   background-color: transparent;
 `;
 
-export const CallToAction = styled(Button)`
+export const CallToAction = styled(Button)<{ isLoading: boolean }>`
+  position: relative;
   background-color: ${({ theme }) => theme.colors.action};
   padding: ${({ theme }) => theme.gaps[300]};
   border-radius: ${({ theme }) => theme.border.radius[500]};
@@ -35,5 +36,40 @@ export const CallToAction = styled(Button)`
 
   :active {
     transform: scale(0.99);
+  }
+
+  ${({ isLoading }) => isLoading && loader};
+`;
+
+const spin = keyframes` 
+  from {
+    transform: translateY(-50%) rotate(0);
+  }
+
+  to {
+    transform: translateY(-50%) rotate(360deg);
+  }
+`;
+
+const loader = css`
+  ::after {
+    content: "";
+
+    position: absolute;
+    right: ${({ theme }) => theme.gaps[300]};
+    top: 50%;
+
+    border: ${({ theme }) => theme.border.sizes[700]} solid;
+    border-color: ${({ theme }) => theme.colors[100]};
+    border-top-color: transparent;
+    border-radius: 50%;
+
+    height: ${({ theme }) => theme.fontSizes[500]};
+    aspect-ratio: 1 / 1;
+
+    animation-name: ${spin};
+    animation-duration: ${({ theme }) => theme.animation.durations[500]};
+    animation-timing-function: ${({ theme }) => theme.animation.timingFunction};
+    animation-iteration-count: infinite;
   }
 `;

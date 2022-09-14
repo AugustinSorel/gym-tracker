1c7ca443ded13d28cb3e537cdb83617104e987a3
AugustinSorel <sorelaugustin@gmail.com>
2022-09-14 17:09:51 +0100

    user all route added

--- src/server/routers/user.ts
+++ src/server/routers/user.ts
@@ -4,27 +4,38 @@ import * as userServices from "../services/userServices";
 import { Prisma } from "@prisma/client";
 import * as bcrypt from "src/utils/bcrypts";
 
-const userRouter = router().mutation("create", {
-  input: userSchemas.create,
-  resolve: async ({ input }) => {
-    try {
-      return await userServices.create({
-        data: {
-          ...input,
-          password: await bcrypt.encryptString(input.password),
-        },
-      });
-    } catch (error) {
-      if (error instanceof Prisma.PrismaClientKnownRequestError) {
-        if (error.code === "P2002") {
-          throw new TRPCError({
-            code: "CONFLICT",
-            message: "email is already used",
-          });
+const userRouter = router()
+  .mutation("create", {
+    input: userSchemas.create,
+    resolve: async ({ input }) => {
+      try {
+        return await userServices.create({
+          data: {
+            ...input,
+            password: await bcrypt.encryptString(input.password),
+          },
+        });
+      } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+          if (error.code === "P2002") {
+            throw new TRPCError({
+              code: "CONFLICT",
+              message: "email is already used",
+            });
+          }
         }
       }
-    }
-  },
-});
+    },
+  })
+  .query("all", {
+    resolve: async () => {
+      try {
+        return await userServices.findMany({});
+      } catch (error) {
+        console.log(error);
+        return error;
+      }
+    },
+  });
 
 export default userRouter;

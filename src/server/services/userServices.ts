9c3e4373f1cc3abe0e64c0bf0225a1c12a041b47
AugustinSorel <sorelaugustin@gmail.com>
2022-09-14 18:03:14 +0100

    user find unique added

--- src/server/services/userServices.ts
+++ src/server/services/userServices.ts
@@ -5,6 +5,10 @@ export const create = (props: Prisma.UserCreateArgs) => {
   return prisma.user.create(props);
 };
 
+export const findUnique = (props: Prisma.UserFindUniqueArgs) => {
+  return prisma.user.findUnique(props);
+};
+
 export const findMany = (props: Prisma.UserFindManyArgs) => {
   return prisma.user.findMany(props);
 };

da6032c6e94a8bf11d8f1b91202dc3094b579382
AugustinSorel <sorelaugustin@gmail.com>
2022-09-14 17:09:43 +0100

    user find many service added

--- src/server/services/userServices.ts
+++ src/server/services/userServices.ts
@@ -4,3 +4,7 @@ import prisma from "src/utils/prisma";
 export const create = (props: Prisma.UserCreateArgs) => {
   return prisma.user.create(props);
 };
+
+export const findMany = (props: Prisma.UserFindManyArgs) => {
+  return prisma.user.findMany(props);
+};

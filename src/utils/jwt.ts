e82df1658fc9ede037bfd4474461784996e82254
AugustinSorel <sorelaugustin@gmail.com>
2022-09-15 19:06:56 +0100

    AccessToken and RefreshToken types added

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,9 +1,17 @@
+import { User } from "@prisma/client";
 import jwt from "jsonwebtoken";
 
 type Key = "ACCESS_TOKEN_KEY" | "REFRESH_TOKEN_KEY";
 
-export const sign = (payload: Object, key: Key) => {
-  return jwt.sign(payload, process.env[key]);
+export type AccessToken = Pick<User, "id">;
+export type RefreshToken = Pick<User, "id">;
+
+export const getRefreshToken = (payload: RefreshToken) => {
+  return jwt.sign(payload, process.env["REFRESH_TOKEN_KEY"]);
+};
+
+export const getAccessToken = (payload: AccessToken) => {
+  return jwt.sign(payload, process.env["ACCESS_TOKEN_KEY"]);
 };
 
 export const verify = <T>(token: string, key: Key) => {

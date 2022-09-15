07172d2972105c3c6e8748cd87ec7d90aa5b8f29
AugustinSorel <sorelaugustin@gmail.com>
2022-09-15 20:54:37 +0100

    Cookies type added

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -1,12 +1,14 @@
 import { NextApiResponse } from "next";
 
+export type Cookies = {
+  accessToken: string;
+  refreshToken: string;
+};
+
 const refreshTokenMaxAge = 60 * 60 * 24 * 7;
 const accessTokenMaxAge = 60 * 15;
 
-export const setAuthCookie = (
-  res: NextApiResponse,
-  tokens: { refreshToken: string; accessToken: string }
-) => {
+export const setAuthCookie = (res: NextApiResponse, tokens: Cookies) => {
   res.setHeader("Set-cookie", [
     `refreshToken=${tokens.refreshToken}; Max-Age=${refreshTokenMaxAge}; Path=/; HttpOnly`,
     `accessToken=${tokens.accessToken}; Max-Age=${accessTokenMaxAge}; Path=/; HttpOnly`,

6e287a9f175d84f7cf6c1f644a0c5a5b4b6ce8ba
AugustinSorel <sorelaugustin@gmail.com>
2022-09-16 13:13:07 +0100

    trpc server added

---
+++ src/server/trpc.ts
@@ -0,0 +1,20 @@
+import { initTRPC } from "@trpc/server";
+import { ZodError } from "zod";
+import { Context } from "./context";
+
+const t = initTRPC.context<Context>().create({
+  errorFormatter: ({ shape, error }) => {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.code === "BAD_REQUEST" && error.cause instanceof ZodError
+            ? error.cause.flatten()
+            : null,
+      },
+    };
+  },
+});
+
+export default t;

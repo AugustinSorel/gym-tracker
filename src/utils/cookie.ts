6d9e32c4d742396f8f07081406f3aebe34f15662
AugustinSorel <sorelaugustin@gmail.com>
2022-09-16 19:24:25 +0100

    resetAuthCookies added

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -8,9 +8,16 @@ export type Cookies = {
 const refreshTokenMaxAge = 60 * 60 * 24 * 7;
 const accessTokenMaxAge = 60 * 15;
 
-export const setAuthCookie = (res: NextApiResponse, tokens: Cookies) => {
+export const setAuthCookies = (res: NextApiResponse, tokens: Cookies) => {
   res.setHeader("Set-cookie", [
     `refreshToken=${tokens.refreshToken}; Max-Age=${refreshTokenMaxAge}; Path=/; HttpOnly`,
     `accessToken=${tokens.accessToken}; Max-Age=${accessTokenMaxAge}; Path=/; HttpOnly`,
+  ]);
+};
+
+export const resetAuthCookies = (res: NextApiResponse) => {
+  res.setHeader("Set-cookie", [
+    "refreshToken=''; Max-Age=-1; Path=/; HttpOnly",
+    "accessToken=''; Max-Age=-1; Path=/; HttpOnly",
   ]);
 };

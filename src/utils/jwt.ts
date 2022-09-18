c3513be26235396116a853de574b180fac96926a
AugustinSorel <sorelaugustin@gmail.com>
2022-09-18 14:10:06 +0100

    max age added to tokens

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,17 +1,21 @@
-import { User } from "@prisma/client";
+import { Session, User } from "@prisma/client";
 import jwt from "jsonwebtoken";
 
 type Key = "ACCESS_TOKEN_KEY" | "REFRESH_TOKEN_KEY";
 
 export type AccessToken = Pick<User, "id">;
-export type RefreshToken = Pick<User, "id">;
+export type RefreshToken = Pick<User, "id"> & Pick<Session, "tokenVersion">;
 
 export const getRefreshToken = (payload: RefreshToken) => {
-  return jwt.sign(payload, process.env["REFRESH_TOKEN_KEY"]);
+  return jwt.sign(payload, process.env["REFRESH_TOKEN_KEY"], {
+    expiresIn: "15min",
+  });
 };
 
 export const getAccessToken = (payload: AccessToken) => {
-  return jwt.sign(payload, process.env["ACCESS_TOKEN_KEY"]);
+  return jwt.sign(payload, process.env["ACCESS_TOKEN_KEY"], {
+    expiresIn: "7d",
+  });
 };
 
 export const verify = <T>(token: string, key: Key) => {

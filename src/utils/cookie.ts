1b7ed939b1d6035aa0aa4b3077774310f2b4184a
AugustinSorel <sorelaugustin@gmail.com>
2022-09-18 17:03:29 +0100

    ServerResponse added to props

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -1,6 +1,7 @@
+import { ServerResponse } from "http";
 import { NextApiResponse } from "next";
 
-export type Cookies = {
+export type AuthCookies = {
   accessToken: string;
   refreshToken: string;
 };
@@ -8,14 +9,17 @@ export type Cookies = {
 const refreshTokenMaxAge = 60 * 60 * 24 * 7;
 const accessTokenMaxAge = 60 * 15;
 
-export const setAuthCookies = (res: NextApiResponse, tokens: Cookies) => {
+export const setAuthCookies = (
+  res: NextApiResponse | ServerResponse,
+  tokens: AuthCookies
+) => {
   res.setHeader("Set-cookie", [
     `refreshToken=${tokens.refreshToken}; Max-Age=${refreshTokenMaxAge}; Path=/; HttpOnly`,
     `accessToken=${tokens.accessToken}; Max-Age=${accessTokenMaxAge}; Path=/; HttpOnly`,
   ]);
 };
 
-export const resetAuthCookies = (res: NextApiResponse) => {
+export const deleteAuthCookies = (res: NextApiResponse | ServerResponse) => {
   res.setHeader("Set-cookie", [
     "refreshToken=''; Max-Age=-1; Path=/; HttpOnly",
     "accessToken=''; Max-Age=-1; Path=/; HttpOnly",

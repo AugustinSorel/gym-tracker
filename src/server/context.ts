df8487d6b76fe58a4b7ac4758ef5bd8162545c92
AugustinSorel <sorelaugustin@gmail.com>
2022-09-18 14:17:24 +0100

    deserialize user in context

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,7 +1,9 @@
 import { CreateNextContextOptions } from "@trpc/server/adapters/next";
+import { deserializeUser } from "src/utils/auth";
 
 export const createContext = ({ req, res }: CreateNextContextOptions) => {
-  return { req, res };
+  const user = deserializeUser(req.cookies);
+  return { req, res, user };
 };
 
 export type Context = ReturnType<typeof createContext>;
--- src/server/middlewares/requireUser.ts
+++ src/server/middlewares/requireUser.ts
@@ -1,15 +1,12 @@
 import { TRPCError } from "@trpc/server";
-import { deserializeUser } from "src/utils/auth";
 import t from "../trpc";
 
 const requireUser = t.middleware(({ ctx, next }) => {
-  const user = deserializeUser(ctx.req.cookies);
-
-  if (!user) {
+  if (!ctx.user) {
     throw new TRPCError({ code: "UNAUTHORIZED" });
   }
 
-  return next({ ctx: { user } });
+  return next({ ctx: { user: ctx.user } });
 });
 
 export default requireUser;

5cc74536d8c5df47d921445edb9cb095ce8c5ad5
AugustinSorel <sorelaugustin@gmail.com>
2022-09-18 19:16:44 +0100

    createContext async

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,9 +1,10 @@
+import { inferAsyncReturnType } from "@trpc/server";
 import { CreateNextContextOptions } from "@trpc/server/adapters/next";
 import { deserializeUser } from "src/utils/auth";
 
-export const createContext = ({ req, res }: CreateNextContextOptions) => {
-  const user = deserializeUser(req.cookies);
+export const createContext = async ({ req, res }: CreateNextContextOptions) => {
+  const user = await deserializeUser(res, req.cookies);
   return { req, res, user };
 };
 
-export type Context = ReturnType<typeof createContext>;
+export type Context = inferAsyncReturnType<typeof createContext>;

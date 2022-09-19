cd7387ec4d9362a94685887e6dd7cae4980f42a2
AugustinSorel <sorelaugustin@gmail.com>
2022-09-19 19:10:09 +0100

    auth router added

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,8 +1,10 @@
 import t from "../trpc";
+import authRouter from "./auth";
 import userRouter from "./user";
 
 export const appRouter = t.router({
   user: userRouter,
+  auth: authRouter,
 });
 
 export type AppRouter = typeof appRouter;

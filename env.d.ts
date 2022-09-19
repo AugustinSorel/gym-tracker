43a709f6b10d38c21a8d34a1b13a1b83f3e6f317
AugustinSorel <sorelaugustin@gmail.com>
2022-09-19 18:24:34 +0100

    NEXT_PUBLIC_GOOGLE_CLIENT_ID secret added

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,7 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      NEXT_PUBLIC_GOOGLE_CLIENT_ID: string;
       DATABASE_URL: string;
       ACCESS_TOKEN_KEY: string;
       REFRESH_TOKEN_KEY: string;

a74dd24b489edcca7eeecba160de2ba2ff1b2ea0
AugustinSorel <sorelaugustin@gmail.com>
2022-09-19 20:06:05 +0100

    next js script removed

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ import Document, {
   Main,
   NextScript,
 } from "next/document";
-import Script from "next/script";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -38,10 +37,11 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@200;300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
-          <Script
+          <script
             src="https://accounts.google.com/gsi/client"
-            strategy="afterInteractive"
-          />
+            async
+            defer
+          ></script>
         </Head>
         <body>
           <Main />

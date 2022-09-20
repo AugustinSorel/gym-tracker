663cc774c8f34cf53e90f543fec8112f4d3c7e11
AugustinSorel <sorelaugustin@gmail.com>
2022-09-20 20:57:55 +0100

    next auth session added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,32 +1,23 @@
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { NextPage } from "next";
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
-import { ReactElement, ReactNode } from "react";
 import trpc from "src/utils/trpc";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "../styles/GlobalStyle.styled";
 import theme from "../styles/theme";
 
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
-
-const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
-  const getLayout = Component.getLayout ?? ((page) => page);
-
+// TODO: create a styled component layout
+const MyApp = ({ Component, pageProps }: AppProps<{ session: Session }>) => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      {getLayout(<Component {...pageProps} />)}
-      <ReactQueryDevtools />
-    </ThemeProvider>
+    <SessionProvider session={pageProps.session}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+        <ReactQueryDevtools />
+      </ThemeProvider>
+    </SessionProvider>
   );
 };
 
-const MyAppWithTrpc = trpc.withTRPC(MyApp);
-
-export default MyAppWithTrpc;
+export default trpc.withTRPC(MyApp);

1ebced47fc61f5c18208a509355fbb59d3326083
AugustinSorel <sorelaugustin@gmail.com>
2022-09-21 12:26:18 +0100

    env added

--- env.d.ts
+++ env.d.ts
@@ -2,8 +2,13 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       DATABASE_URL: string;
-      ACCESS_TOKEN_KEY: string;
-      REFRESH_TOKEN_KEY: string;
+      GOOGLE_CLIENT_SECRET: string;
+      GOOGLE_CLIENT_ID: string;
+      EMAIL_SERVER_USER: string;
+      EMAIL_SERVER_PASSWORD: string;
+      EMAIL_SERVER_HOST: string;
+      EMAIL_SERVER_PORT: number;
+      EMAIL_FROM: string;
       NODE_ENV: "development" | "production";
       PORT?: string;
     }

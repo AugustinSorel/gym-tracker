bf635f7121cd84497a7f029d39e82e7ceaf24615
AugustinSorel <sorelaugustin@gmail.com>
2022-09-21 12:26:59 +0100

    auth provider added

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,32 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "src/utils/prisma";
+import EmailProvider from "next-auth/providers/email";
+
+const nextAuthOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    // TODO:  try with other way
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+  pages: {
+    signIn: "/authentication",
+  },
+};
+
+export default NextAuth(nextAuthOptions);

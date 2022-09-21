5e327ca07ce39790cf36fd7ea7863b98ea6a4d92
AugustinSorel <sorelaugustin@gmail.com>
2022-09-21 18:17:31 +0100

    get session in context

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,9 +1,12 @@
 import { inferAsyncReturnType } from "@trpc/server";
 import { CreateNextContextOptions } from "@trpc/server/adapters/next";
+import { unstable_getServerSession } from "next-auth";
+import { nextAuthOptions } from "src/pages/api/auth/[...nextauth]";
 
 export const createContext = async ({ req, res }: CreateNextContextOptions) => {
-  const user = null;
-  return { req, res, user };
+  const session = await unstable_getServerSession(req, res, nextAuthOptions);
+
+  return { req, res, session };
 };
 
 export type Context = inferAsyncReturnType<typeof createContext>;

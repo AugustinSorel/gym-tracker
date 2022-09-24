1a56439a72e20707934caaf4d0fa5f1dc5fcc332
AugustinSorel <sorelaugustin@gmail.com>
2022-09-24 18:03:37 +0100

    auth errors added

---
+++ src/utils/auth.ts
@@ -0,0 +1,14 @@
+export const AUTH_ERRORS = {
+  Signin: "Try signing with a different account.",
+  OAuthSignin: "Try signing with a different account.",
+  OAuthCallback: "Try signing with a different account.",
+  OAuthCreateAccount: "Try signing with a different account.",
+  EmailCreateAccount: "Try signing with a different account.",
+  Callback: "Try signing with a different account.",
+  OAuthAccountNotLinked:
+    "Try using another provider. This account is regesitered with a different provider",
+  EmailSignin: "Check your email address.",
+  CredentialsSignin:
+    "Sign in failed. Check the details you provided are correct.",
+  default: "Unable to sign in.",
+};

bd0c46e3b0636f85acc98b31d61faa2ce5fe6a07
Augustin <48162609+AugustinSorel@users.noreply.github.com>
2022-09-27 13:32:28 +0100

    Add new exercise (#36)
    * exercise router added
    
    * exerciseRouter added to appRouter
    
    * newExerciseName schema
    
    * newExerciseMutation added
    
    * exercise prisma added

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,8 +1,10 @@
 import t from "../trpc";
+import exerciseRouter from "./exercise";
 import userRouter from "./user";
 
 export const appRouter = t.router({
   user: userRouter,
+  exercise: exerciseRouter,
 });
 
 export type AppRouter = typeof appRouter;

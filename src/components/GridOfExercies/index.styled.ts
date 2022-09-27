6157e8a0c9bdbbe0aa914edd14df8e5b3adc65a9
AugustinSorel <sorelaugustin@gmail.com>
2022-09-27 16:34:52 +0100

    grid skeleton added

--- src/components/GridOfExercies/index.styled.ts
+++ src/components/GridOfExercies/index.styled.ts
@@ -7,6 +7,45 @@ export const Grid = styled.main`
   margin-top: ${({ theme }) => theme.gaps[900]};
 `;
 
+const lineMove = keyframes`
+  from {
+    left: calc(var(--width) * -1);
+  }
+
+  to {
+    left: 100%;
+  }
+`;
+
+export const GridSkeleton = styled(Grid)`
+  position: relative;
+  overflow: hidden;
+
+  &::after {
+    content: "";
+    position: absolute;
+    background-color: ${({ theme }) => theme.colors[400]};
+    opacity: 0.2;
+
+    --width: 10%;
+
+    background: linear-gradient(
+      to right,
+      transparent 0%,
+      ${({ theme }) => theme.colors[500]} 50%,
+      transparent 100%
+    );
+    top: 0;
+    height: 100%;
+    width: var(--width);
+
+    animation-name: ${lineMove};
+    animation-duration: ${({ theme }) => theme.animation.durations[500]};
+    animation-timing-function: ${({ theme }) => theme.animation.timingFunction};
+    animation-iteration-count: infinite;
+  }
+`;
+
 const fade = keyframes`
   from {
     opacity: 0;
@@ -47,4 +86,16 @@ export const Item = styled.div<{ delay: number }>`
   &:active {
     transform: scale(0.98);
   }
+`;
+
+export const ItemSkeleton = styled.div`
+  background-color: ${({ theme }) => theme.colors[300]};
+  aspect-ratio: 1;
+  cursor: pointer;
+  outline: none;
+`;
+
+export const Title = styled.h2`
+  text-overflow: ellipsis;
+  overflow: hidden;
 `;

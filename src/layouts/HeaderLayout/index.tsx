124d815ac6f6d0c888722a06c9c8738c1697b7a5
AugustinSorel <sorelaugustin@gmail.com>
2022-09-27 12:34:15 +0100

    signing out handler added

--- src/layouts/HeaderLayout/index.tsx
+++ src/layouts/HeaderLayout/index.tsx
@@ -1,12 +1,18 @@
-import { useSession } from "next-auth/react";
+import Button from "@/components/Button";
+import { signOut, useSession } from "next-auth/react";
 import * as Styles from "./index.styled";
 
 const HeaderLayout = () => {
   const { data: session } = useSession();
+
+  const signOutHandler = () => {
+    signOut({ redirect: true, callbackUrl: "/sign-in" });
+  };
+
   return (
     <Styles.Header>
       <Styles.Title>gym tracker</Styles.Title>
-      {session?.user?.name && <Styles.Name>{session.user.name}</Styles.Name>}
+      <Button role="default" text="sign-out" onClick={signOutHandler} />
       {session?.user?.image && <Styles.Avatar src={session.user.image} />}
     </Styles.Header>
   );

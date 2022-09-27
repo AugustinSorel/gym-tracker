0c772b923bff75f9db22215c148824c861d43d2d
AugustinSorel <sorelaugustin@gmail.com>
2022-09-27 13:20:26 +0100

    exercise router added

---
+++ src/server/routers/exercise.ts
@@ -0,0 +1,33 @@
+import t from "../trpc";
+import * as exerciseSchemas from "@/schemas/exerciseSchema";
+import prisma from "src/utils/prisma";
+import requireUser from "../middlewares/requireUser";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import { TRPCError } from "@trpc/server";
+
+const exerciseRouter = t.router({
+  new: t.procedure
+    .use(requireUser)
+    .input(exerciseSchemas.newExerciseName)
+    .mutation(async ({ input, ctx }) => {
+      try {
+        return await prisma.exercise.create({
+          data: {
+            name: input,
+            User: { connect: { id: ctx.user.id } },
+          },
+        });
+      } catch (error) {
+        if (error instanceof PrismaClientKnownRequestError) {
+          if (error.code === "P2002") {
+            throw new TRPCError({
+              code: "CONFLICT",
+              message: "This name is already used",
+            });
+          }
+        }
+      }
+    }),
+});
+
+export default exerciseRouter;

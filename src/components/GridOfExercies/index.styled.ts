7902b80608396c60e401c89c8d0cea67807b6272
Augustin <48162609+AugustinSorel@users.noreply.github.com>
2022-09-28 18:07:45 +0100

    Card style (#38)
    * menu icon added
    
    * item header added
    
    * border radius  added
    
    * dependencies updated
    
    * dependencies updated
    
    * dependencies updated
    
    * recharts bug fixed
    
    * padding removed from graph
    
    * dummy graph added
    
    * grid column changed
    
    * header background lighter

--- src/components/GridOfExercies/index.styled.ts
+++ src/components/GridOfExercies/index.styled.ts
@@ -5,8 +5,7 @@ export const Grid = styled.main`
   margin-top: ${({ theme }) => theme.gaps[900]};
 
   display: grid;
-  place-content: center;
-  grid-template-columns: repeat(auto-fit, 300px);
+  grid-template-columns: repeat(4, 1fr);
 
   @media ${({ theme }) => theme.breakpoints.tablet} {
     grid-template-columns: repeat(auto-fit, 100%);
@@ -72,6 +71,11 @@ export const Item = styled.div<{ delay: number }>`
   aspect-ratio: 1;
   cursor: pointer;
   outline: none;
+  border-radius: ${({ theme }) => theme.border.radius[300]};
+  overflow: hidden;
+
+  display: flex;
+  flex-direction: column;
 
   animation: ${scaleUp}, ${fade} both;
   animation-duration: ${({ theme }) => theme.animation.durations[300]};
@@ -88,6 +92,10 @@ export const Item = styled.div<{ delay: number }>`
     background-color: ${({ theme }) => theme.colors[400]};
   }
 
+  &:hover svg {
+    opacity: 1;
+  }
+
   &:active {
     transform: scale(0.98);
   }
@@ -96,11 +104,45 @@ export const Item = styled.div<{ delay: number }>`
 export const ItemSkeleton = styled.div`
   background-color: ${({ theme }) => theme.colors[300]};
   aspect-ratio: 1;
-  cursor: pointer;
-  outline: none;
+  border-radius: ${({ theme }) => theme.border.radius[300]};
+  overflow: hidden;
+
+  display: flex;
+  flex-direction: column;
+
+  & > * {
+    padding: ${({ theme }) => theme.gaps[200]};
+  }
 `;
 
-export const Title = styled.h2`
+export const ExerciseName = styled.h2`
   text-overflow: ellipsis;
   overflow: hidden;
+  font-weight: 400;
+  font-size: ${({ theme }) => theme.fontSizes[600]};
+  flex: 1;
+`;
+
+export const Header = styled.header`
+  background-color: ${({ theme }) => theme.colors[400]};
+  display: flex;
+  align-items: center;
+  gap: ${({ theme }) => theme.gaps[200]};
+  fill: currentColor;
+  padding: ${({ theme }) => theme.gaps[200]};
+
+  svg {
+    opacity: 0;
+    cursor: move;
+  }
+`;
+
+export const HeaderSkeleton = styled(Header)`
+  height: ${({ theme }) => theme.fontSizes[900]};
+`;
+
+export const Graph = styled.div`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors[400]};
+  margin: ${({ theme }) => theme.gaps[900]};
 `;
--- src/components/GridOfExercies/index.tsx
+++ src/components/GridOfExercies/index.tsx
@@ -1,13 +1,18 @@
 import { Exercise } from "@prisma/client";
 import { memo, useRef } from "react";
+import { Line, LineChart, ResponsiveContainer } from "recharts";
+import theme from "src/styles/theme";
 import trpc from "src/utils/trpc";
+import SvgIcon from "../SvgIcon";
 import * as Styles from "./index.styled";
 
 const GridSkeleton = () => {
   return (
     <Styles.GridSkeleton>
       {[...Array(10)].map((_, i) => (
-        <Styles.ItemSkeleton key={i} />
+        <Styles.ItemSkeleton key={i}>
+          <Styles.HeaderSkeleton />
+        </Styles.ItemSkeleton>
       ))}
     </Styles.GridSkeleton>
   );
@@ -17,6 +22,41 @@ const Grid = memo(({ exercises }: { exercises: Exercise[] }) => {
   const renderCounter = useRef(0);
   renderCounter.current += 1;
 
+  const getRandomValue = () => {
+    return Math.random() * 100;
+  };
+
+  const data = () => [
+    {
+      name: "Page A",
+      pv: getRandomValue(),
+    },
+    {
+      name: "Page B",
+      pv: getRandomValue(),
+    },
+    {
+      name: "Page C",
+      pv: getRandomValue(),
+    },
+    {
+      name: "Page D",
+      pv: getRandomValue(),
+    },
+    {
+      name: "Page E",
+      pv: getRandomValue(),
+    },
+    {
+      name: "Page F",
+      pv: getRandomValue(),
+    },
+    {
+      name: "Page G",
+      pv: getRandomValue(),
+    },
+  ];
+
   return (
     <Styles.Grid>
       {exercises.map((exercise, i) => (
@@ -25,7 +65,25 @@ const Grid = memo(({ exercises }: { exercises: Exercise[] }) => {
           key={exercise.id}
           delay={i * 50 * (renderCounter.current > 1 ? 0 : 1)}
         >
-          <Styles.Title>{exercise.name}</Styles.Title>
+          <Styles.Header>
+            <Styles.ExerciseName>{exercise.name}</Styles.ExerciseName>
+            <SvgIcon svgName="menu" />
+          </Styles.Header>
+
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart
+              data={data()}
+              margin={{ bottom: 10, left: 10, right: 10, top: 10 }}
+            >
+              <Line
+                type="monotone"
+                dataKey="pv"
+                stroke={theme.colors.action}
+                strokeWidth={1}
+                dot={false}
+              />
+            </LineChart>
+          </ResponsiveContainer>
         </Styles.Item>
       ))}
     </Styles.Grid>

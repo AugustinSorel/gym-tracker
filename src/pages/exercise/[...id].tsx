80b0c7f02d67d6ca755ad72fe46b49a62b0b5d40
AugustinSorel <sorelaugustin@gmail.com>
2022-09-28 20:18:02 +0100

    layouts added to page

--- src/pages/exercise/[...id].tsx
+++ src/pages/exercise/[...id].tsx
@@ -1,9 +1,24 @@
 import { useRouter } from "next/router";
+import HeaderLayout from "src/layouts/HeaderLayout";
+import MaxWidthLayout from "src/layouts/MaxWidthLayout";
+import PriveRouteLayout from "src/layouts/PrivateRouteLayout";
+import { NextPageWithLayout } from "../_app";
 
-const ExercisePage = () => {
+const ExercisePage: NextPageWithLayout = () => {
   const router = useRouter();
 
   return <h1>{router.query.id}</h1>;
+};
+
+ExercisePage.getLayout = (page) => {
+  return (
+    <PriveRouteLayout>
+      <MaxWidthLayout>
+        <HeaderLayout />
+        {page}
+      </MaxWidthLayout>
+    </PriveRouteLayout>
+  );
 };
 
 export default ExercisePage;

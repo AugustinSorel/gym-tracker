0152f1e35b899f8fad5e6546332ace0f0fa91b76
AugustinSorel <sorelaugustin@gmail.com>
2022-09-29 18:50:41 +0100

    exercise history added

---
+++ src/components/ExerciseHistory/index.tsx
@@ -0,0 +1,11 @@
+import * as Styles from "./index.styled";
+
+const ExerciseHistory = () => {
+  return (
+    <Styles.Container>
+      <p>history</p>
+    </Styles.Container>
+  );
+};
+
+export default ExerciseHistory;

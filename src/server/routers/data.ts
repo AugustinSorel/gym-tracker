bbafbc1acb8441e87862dee12a3667ad93b0457c
AugustinSorel <sorelaugustin@gmail.com>
2022-09-30 14:55:37 +0100

    upsert new data

--- src/server/routers/data.ts
+++ src/server/routers/data.ts
@@ -1,7 +1,10 @@
+import * as dataSchema from "@/schemas/dataSchema";
+import { TRPCError } from "@trpc/server";
+import { getCurrentDateFormated } from "src/utils/date";
+import { getOneRepMax } from "src/utils/math";
 import prisma from "src/utils/prisma";
 import requireUser from "../middlewares/requireUser";
 import t from "../trpc";
-import * as dataSchema from "@/schemas/dataSchema";
 
 const dataRouter = t.router({
   new: t.procedure
@@ -9,21 +12,30 @@ const dataRouter = t.router({
     .input(dataSchema.addData)
     .mutation(async ({ input }) => {
       const { numberOfReps, weight, exerciseName } = input;
-      const oneRepMax = weight * (1 + numberOfReps / 30);
-      console.log(input);
+
+      const oneRepMax = getOneRepMax(numberOfReps, weight);
+      const createdAt = getCurrentDateFormated();
+
       try {
-        const data = await prisma.data.create({
-          data: {
+        const data = await prisma.data.upsert({
+          where: { createdAt },
+          create: {
             numberOfReps,
             weight,
             oneRepMax,
             exercise: { connect: { name: exerciseName } },
+            createdAt,
           },
+
+          update: { numberOfReps, weight, oneRepMax },
         });
 
         return data;
       } catch (error) {
-        console.log(error);
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Something went wrong",
+        });
       }
     }),
 });

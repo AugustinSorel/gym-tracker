5a7bd16e385663ecfa12dce30ce3444772f9eebb
AugustinSorel <sorelaugustin@gmail.com>
2022-09-30 12:53:51 +0100

    data schema added

--- src/schemas/exerciseSchema.ts
+++ src/schemas/exerciseSchema.ts
@@ -6,21 +6,3 @@ export const newExerciseName = z
   })
   .min(3, "minimum lenght is 3")
   .max(255, "maximum length is 255");
-
-export const addExerciseData = z.object({
-  numberOfReps: z
-    .number({
-      required_error: "number of reps are required",
-      invalid_type_error: "number of reps must be a number",
-    })
-    .positive({ message: "number of reps must be a positive number" })
-    .lt(40, { message: "number of reps must be smaller than 40" }),
-
-  weightLifted: z
-    .number({
-      required_error: "weight lifted is required",
-      invalid_type_error: "weight lifted must be a number",
-    })
-    .positive({ message: "weight lifted must be a positive number" })
-    .lt(500, { message: "weight lifted must be smaller than 500 kg" }),
-});

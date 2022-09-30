990af35b25394a0c17f102085b957a6ab46ac775
AugustinSorel <sorelaugustin@gmail.com>
2022-09-30 20:55:28 +0100

    time frame dict added

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,4 +1,5 @@
-// DATE FORMAT - YEAR-MM-DD
+import { TimeFrame } from "@/schemas/exerciseSchema";
+
 export const getCurrentDate = () => {
   return new Date(
     new Date().getFullYear(),
@@ -13,4 +14,29 @@ export const getDateInFrenchFormat = (date: string) => {
     month: "2-digit",
     year: "2-digit",
   });
+};
+
+const addMonths = (date: Date, months: number) => {
+  date.setMonth(date.getMonth() + months);
+  return date;
+};
+
+const addDays = (date: Date, days: number) => {
+  date.setDate(date.getDate() + days);
+  return date;
+};
+
+const addYears = (date: Date, years: number) => {
+  date.setFullYear(date.getFullYear() + years);
+  return date;
+};
+
+export const timeFrameDict: {
+  [key in TimeFrame]: any;
+} = {
+  ALL: undefined,
+  "1Y": addYears(new Date(), -1),
+  "6M": addMonths(new Date(), -6),
+  "1M": addMonths(new Date(), -1),
+  "1W": addDays(new Date(), -7),
 };

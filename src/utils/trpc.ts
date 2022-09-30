9b412370089a053b7e524e5adab81f8134621ec2
AugustinSorel <sorelaugustin@gmail.com>
2022-09-30 18:55:37 +0100

    super json added

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,10 +1,12 @@
 import { httpBatchLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 import { AppRouter } from "src/server/routers/_app";
+import superjson from "superjson";
 
 const trpc = createTRPCNext<AppRouter>({
   config: () => {
     return {
+      transformer: superjson,
       links: [httpBatchLink({ url: "/api/trpc" })],
     };
   },

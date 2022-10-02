eff8384bb746493c78023674a9aa57328cc779d4
AugustinSorel <sorelaugustin@gmail.com>
2022-10-02 21:36:38 +0100

    basic history added

--- src/features/ExerciseDetails/ExerciseHistory.tsx
+++ src/features/ExerciseDetails/ExerciseHistory.tsx
@@ -1,10 +1,48 @@
+import { TimeFrame } from "@/schemas/exerciseSchema";
+import { getDateInFrenchFormat } from "src/utils/date";
+import { TwoDigitsNumber } from "src/utils/math";
+import { InferProcedures } from "src/utils/trpc";
 import * as Styles from "./ExerciseHistory.styled";
 
-const ExerciseHistory = () => {
+type Props = {
+  isLoading: boolean;
+  timeFrame: TimeFrame;
+  exercise?: InferProcedures["exercise"]["get"]["output"];
+};
+
+const ExerciseHistory = ({ exercise, isLoading, timeFrame }: Props) => {
+  if (isLoading || !exercise) {
+    return (
+      <Styles.ListSkeleton>
+        {[...Array(20)].map((_, i) => (
+          <Styles.ListItemSkeleton key={i} />
+        ))}
+      </Styles.ListSkeleton>
+    );
+  }
+
   return (
-    <Styles.Container>
-      <p>history</p>
-    </Styles.Container>
+    <Styles.List>
+      <Styles.ListItem delay={0}>
+        <Styles.OneRepMax>PR</Styles.OneRepMax>
+        <Styles.OneRepMax>Nº</Styles.OneRepMax>
+        <Styles.OneRepMax>Kg</Styles.OneRepMax>
+        <Styles.Date>Date</Styles.Date>
+      </Styles.ListItem>
+
+      {exercise.Data.reverse().map((data, i) => (
+        <Styles.ListItem key={timeFrame + data.id} delay={i * 50}>
+          <Styles.OneRepMax>{TwoDigitsNumber(data.oneRepMax)}</Styles.OneRepMax>
+          <Styles.NumberOfReps>
+            {TwoDigitsNumber(data.numberOfReps)}
+          </Styles.NumberOfReps>
+          <Styles.Weight>{TwoDigitsNumber(data.weight)}</Styles.Weight>
+          <Styles.Date>
+            {getDateInFrenchFormat(data.createdAt.toLocaleDateString())}
+          </Styles.Date>
+        </Styles.ListItem>
+      ))}
+    </Styles.List>
   );
 };
 
--- src/features/ExerciseDetails/index.tsx
+++ src/features/ExerciseDetails/index.tsx
@@ -1,12 +1,48 @@
+import { TimeFrame } from "@/schemas/exerciseSchema";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { trpc } from "src/utils/trpc";
 import ExerciseGraph from "./ExerciseGraph";
 import ExerciseHistory from "./ExerciseHistory";
 import * as Styles from "./index.styled";
 
 const ExerciseDetails = () => {
+  const utils = trpc.useContext();
+  const router = useRouter();
+  const exerciseName = router.query.exerciseName as string;
+  const [selectedTimeFrame, setSelectedTimeFrame] = useState<TimeFrame>("1M");
+
+  const dataQuery = trpc.exercise.get.useQuery(
+    {
+      exerciseName,
+      timeFrame: selectedTimeFrame,
+    },
+    {
+      placeholderData: () => {
+        if (selectedTimeFrame === "1M") {
+          return utils.exercise.all
+            .getData()
+            ?.find((d) => d.name === exerciseName);
+        }
+
+        return undefined;
+      },
+    }
+  );
+
   return (
     <Styles.Main>
-      <ExerciseHistory />
-      <ExerciseGraph />
+      <ExerciseHistory
+        exercise={dataQuery.data}
+        isLoading={dataQuery.isLoading}
+        timeFrame={selectedTimeFrame}
+      />
+      <ExerciseGraph
+        exercise={dataQuery.data}
+        isLoading={dataQuery.isLoading}
+        selectedTimeFrame={selectedTimeFrame}
+        setSelectedTimeFrame={setSelectedTimeFrame}
+      />
     </Styles.Main>
   );
 };

7ae18f17d220e8d2b25b24291b2a30370ef6f4f3
AugustinSorel <sorelaugustin@gmail.com>
2022-10-02 14:45:07 +0100

    grid of exercises added to features

---
+++ src/features/GridOfExercies/GridItem.tsx
@@ -0,0 +1,56 @@
+import Link from "next/link";
+import { Legend, Line, LineChart, ResponsiveContainer } from "recharts";
+import theme from "src/styles/theme";
+import { InferProcedures } from "src/utils/trpc";
+import * as Styles from "./GridItem.styled";
+
+type Props = {
+  exercise: InferProcedures["exercise"]["get"]["output"];
+  delay: number;
+};
+
+// TODO: Make a graph component ?
+const GridItem = ({ exercise, delay }: Props) => {
+  if (!exercise) {
+    return null;
+  }
+
+  return (
+    <Link href={`/exercise/${exercise.name}`} passHref>
+      <Styles.Anchor delay={delay}>
+        <Styles.Container>
+          <Styles.Header>
+            <Styles.ExerciseName>{exercise.name}</Styles.ExerciseName>
+          </Styles.Header>
+
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart
+              data={exercise.Data}
+              margin={{ bottom: 10, left: 10, right: 10, top: 10 }}
+            >
+              <Line
+                type="monotone"
+                dataKey="oneRepMax"
+                stroke={theme.colors.action}
+                strokeWidth={2}
+                dot={false}
+                isAnimationActive={false}
+              />
+              <Line
+                type="monotone"
+                dataKey="predictedOneRepMax"
+                stroke={theme.colors[400]}
+                strokeWidth={2}
+                dot={false}
+                isAnimationActive={false}
+              />
+              <Legend />
+            </LineChart>
+          </ResponsiveContainer>
+        </Styles.Container>
+      </Styles.Anchor>
+    </Link>
+  );
+};
+
+export default GridItem;
---
+++ src/features/GridOfExercies/index.tsx
@@ -0,0 +1,35 @@
+import { useRef } from "react";
+import { trpc } from "src/utils/trpc";
+import GridItem from "./GridItem";
+import GridSkeleton from "./GridSkeleton";
+import * as Styles from "./index.styled";
+
+const GridOfExercises = () => {
+  const exercisesQuery = trpc.exercise.all.useQuery();
+  const gridRef = useRef<HTMLDivElement>(null);
+
+  if (exercisesQuery.isLoading || !exercisesQuery.data) {
+    return <GridSkeleton />;
+  }
+
+  // TODO: Something better ?
+  if (exercisesQuery.data.length < 1) {
+    return <p>No data</p>;
+  }
+
+  return (
+    <Styles.Grid ref={gridRef}>
+      {exercisesQuery.data.map((exercise, i) => {
+        return (
+          <GridItem
+            key={exercise.id}
+            delay={i * 50 * (gridRef.current?.childNodes ? 0 : 1)}
+            exercise={exercise}
+          />
+        );
+      })}
+    </Styles.Grid>
+  );
+};
+
+export default GridOfExercises;

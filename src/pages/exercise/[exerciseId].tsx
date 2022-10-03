812c55e1fc6d3eda50c0dd731bce1a44ebb5c82d
AugustinSorel <sorelaugustin@gmail.com>
2022-10-03 22:24:10 +0100

    exercise id added for route

--- src/pages/exercise/[exerciseId].tsx
+++ src/pages/exercise/[exerciseId].tsx
@@ -7,16 +7,34 @@ import ExerciseDetails from "src/features/ExerciseDetails";
 import HeaderLayout from "src/layouts/HeaderLayout";
 import MaxWidthLayout from "src/layouts/MaxWidthLayout";
 import PriveRouteLayout from "src/layouts/PrivateRouteLayout";
+import useSelectedTimeFrame from "src/store/useSelectedTimeFrame";
+import { trpc } from "src/utils/trpc";
 import { NextPageWithLayout } from "../_app";
 
 const ExercisePage: NextPageWithLayout = () => {
   const router = useRouter();
   const modal = useModal();
+  const utils = trpc.useContext();
+  const exerciseId = router.query.exerciseId as string;
+  const { timeFrame } = useSelectedTimeFrame();
+
+  trpc.exercise.get.useQuery(
+    { exerciseId, timeFrame },
+    {
+      initialData: () => {
+        if (timeFrame === "1M") {
+          return utils.exercise.all.getData()?.find((d) => d.id === exerciseId);
+        }
+
+        return undefined;
+      },
+    }
+  );
 
   return (
     <>
       <Head>
-        <title>Exercise {router.query.id}</title>
+        <title>Exercise</title>
         <meta
           name="description"
           content="View your gym progress with the gym tracker app"

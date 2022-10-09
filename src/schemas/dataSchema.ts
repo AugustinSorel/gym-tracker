6356623bbfd2197518b327fec4de4bd7b34b7e91
AugustinSorel <sorelaugustin@gmail.com>
2022-10-09 19:03:35 +0100

    update data schema

--- src/schemas/dataSchema.ts
+++ src/schemas/dataSchema.ts
@@ -1,21 +1,34 @@
 import z from "zod";
 import { exerciseId } from "./exerciseSchema";
 
+const id = z
+  .string({ required_error: "new exercise name is required" })
+  .cuid({ message: "exercise id must be in the cuid format" });
+
+const weight = z
+  .number({
+    required_error: "weight lifted is required",
+    invalid_type_error: "weight lifted must be a number",
+  })
+  .positive({ message: "weight lifted must be a positive number" })
+  .lt(500, { message: "weight lifted must be smaller than 500 kg" });
+
+const numberOfReps = z
+  .number({
+    required_error: "number of reps are required",
+    invalid_type_error: "number of reps must be a number",
+  })
+  .positive({ message: "number of reps must be a positive number" })
+  .lt(40, { message: "number of reps must be smaller than 40" });
+
 export const addData = z.object({
   exerciseId,
-  numberOfReps: z
-    .number({
-      required_error: "number of reps are required",
-      invalid_type_error: "number of reps must be a number",
-    })
-    .positive({ message: "number of reps must be a positive number" })
-    .lt(40, { message: "number of reps must be smaller than 40" }),
+  numberOfReps,
+  weight,
+});
 
-  weight: z
-    .number({
-      required_error: "weight lifted is required",
-      invalid_type_error: "weight lifted must be a number",
-    })
-    .positive({ message: "weight lifted must be a positive number" })
-    .lt(500, { message: "weight lifted must be smaller than 500 kg" }),
+export const updateData = z.object({
+  id,
+  weight,
+  numberOfReps,
 });

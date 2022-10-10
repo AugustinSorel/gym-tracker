4f1c81ff4b390f78102b455ef42940aeaa6336a0
AugustinSorel <sorelaugustin@gmail.com>
2022-10-10 20:24:38 +0100

    grid item added

--- src/features/GridOfExercies/GridItem.tsx
+++ src/features/GridOfExercies/GridItem.tsx
@@ -1,47 +1,21 @@
-import { ResponsiveLine } from "@nivo/line";
-import Link from "next/link";
-import theme from "src/styles/theme";
-import serializeGraphData from "src/utils/graph";
-import { InferProcedures } from "src/utils/trpc";
+import { ReactNode } from "react";
 import * as Styles from "./GridItem.styled";
 
-//FIXME: removed unused dependencies
-
 type Props = {
-  exercise: NonNullable<InferProcedures["exercise"]["get"]["output"]>;
+  children: ReactNode;
+  title: string;
   delay: number;
 };
 
-const GridItem = ({ exercise, delay }: Props) => {
+const GridItem = ({ title, children, delay }: Props) => {
   return (
-    <Link href={`/exercise/${exercise.id}`} passHref>
-      <Styles.Anchor delay={delay}>
-        <Styles.Container>
-          <Styles.Header>
-            <Styles.ExerciseName>{exercise.name}</Styles.ExerciseName>
-          </Styles.Header>
+    <Styles.Container delay={delay} tabIndex={0}>
+      <Styles.Header>
+        <Styles.ExerciseName>{title}</Styles.ExerciseName>
+      </Styles.Header>
 
-          {exercise.data.length > 0 && (
-            <div>
-              <ResponsiveLine
-                data={serializeGraphData(exercise.data)}
-                margin={{ bottom: 10, left: 10, right: 10, top: 10 }}
-                xScale={{ type: "time", format: "%Y-%m-%d" }}
-                xFormat="time:%Y-%m-%d"
-                yScale={{ type: "linear", min: "auto", max: "auto" }}
-                axisBottom={{ tickSize: 0, format: "" }}
-                axisLeft={{ renderTick: () => <></> }}
-                enableGridX={false}
-                enableGridY={false}
-                enablePoints={exercise.data.length === 1}
-                colors={theme.colors.action}
-                curve="catmullRom"
-              />
-            </div>
-          )}
-        </Styles.Container>
-      </Styles.Anchor>
-    </Link>
+      {children}
+    </Styles.Container>
   );
 };
 

5de8ba33c8b2d4ce2d2bbbfb35723387df345344
AugustinSorel <sorelaugustin@gmail.com>
2022-10-10 20:30:56 +0100

    wrapper added to line graph

--- src/features/GridOfExercies/LineGraph.tsx
+++ src/features/GridOfExercies/LineGraph.tsx
@@ -13,20 +13,22 @@ const LineGraph = ({ exercise }: Props) => {
   }
 
   return (
-    <ResponsiveLine
-      data={serializeLineGraphData(exercise.data)}
-      margin={{ bottom: 50, left: 10, right: 10, top: 10 }}
-      xScale={{ type: "time", format: "%Y-%m-%d" }}
-      xFormat="time:%Y-%m-%d"
-      yScale={{ type: "linear", min: "auto", max: "auto" }}
-      axisBottom={{ tickSize: 0, format: "" }}
-      axisLeft={{ renderTick: () => <></> }}
-      enableGridX={false}
-      enableGridY={false}
-      enablePoints={exercise.data.length === 1}
-      colors={theme.colors.action}
-      curve="catmullRom"
-    />
+    <div style={{ height: "87%", width: "100%" }}>
+      <ResponsiveLine
+        data={serializeLineGraphData(exercise.data)}
+        margin={{ bottom: 10, left: 10, right: 10, top: 10 }}
+        xScale={{ type: "time", format: "%Y-%m-%d" }}
+        xFormat="time:%Y-%m-%d"
+        yScale={{ type: "linear", min: "auto", max: "auto" }}
+        axisBottom={{ tickSize: 0, format: "" }}
+        axisLeft={{ renderTick: () => <></> }}
+        enableGridX={false}
+        enableGridY={false}
+        enablePoints={exercise.data.length === 1}
+        colors={theme.colors.action}
+        curve="catmullRom"
+      />
+    </div>
   );
 };
 

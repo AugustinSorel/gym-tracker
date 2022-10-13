a2b235491fffd058a75b9757b0b0df123dd40e54
AugustinSorel <sorelaugustin@gmail.com>
2022-10-13 21:13:47 +0100

    blue tap removed

--- src/styles/GlobalStyle.styled.ts
+++ src/styles/GlobalStyle.styled.ts
@@ -7,12 +7,15 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
+
+
   body {
     background-color: ${({ theme }) => theme.colors[100]};
     color: ${({ theme }) => theme.colors[900]};
     font-family: 'Noto Sans', sans-serif;
     font-size: ${({ theme }) => theme.fontSizes[500]};
     overflow-x: hidden;
+    -webkit-tap-highlight-color: transparent;
 
     @media ${({ theme }) => theme.breakpoints.tablet} {
       font-size: ${({ theme }) => theme.fontSizes[400]};

7c344b0e130a7aeb316ba82f55565461a76b0a81
AugustinSorel <sorelaugustin@gmail.com>
2022-10-21 13:26:37 +0100

    update handler added

--- src/features/ExerciseDetails/ExerciseGraph.tsx
+++ src/features/ExerciseDetails/ExerciseGraph.tsx
@@ -1,8 +1,9 @@
 import Button from "@/components/Button";
-import { TIME_FRAME_ENUM } from "@/schemas/exerciseSchema";
+import Input from "@/components/Input";
+import { TIME_FRAME_ENUM, updateName } from "@/schemas/exerciseSchema";
 import { useRouter } from "next/router";
 import useSelectedTimeFrame from "src/store/useSelectedTimeFrame";
-import { trpc } from "src/utils/trpc";
+import { InferProcedures, trpc } from "src/utils/trpc";
 import * as Styles from "./ExerciseGraph.styled";
 import LineGraph from "./LineGraph";
 import useGetSelectedExercise from "./useGetSelectedExercise";
@@ -32,6 +33,42 @@ const ExerciseGraph = () => {
     },
   });
 
+  const updateMutation = trpc.exercise.updateName.useMutation({
+    onSettled: () => {
+      utils.exercise.get.invalidate();
+    },
+
+    onMutate: async ({ exerciseId, exerciseName }) => {
+      await utils.exercise.get.cancel();
+
+      const currentExercise = utils.exercise.get.getData({
+        exerciseId,
+        timeFrame,
+      });
+
+      if (!currentExercise) {
+        return;
+      }
+
+      utils.exercise.get.setData(
+        { ...currentExercise, name: exerciseName },
+        { exerciseId, timeFrame }
+      );
+    },
+  });
+
+  const updateHandler = (
+    updatedData: InferProcedures["exercise"]["updateName"]["input"]
+  ) => {
+    const { success } = updateName.safeParse(updatedData);
+
+    if (!success) {
+      throw new Error("invalid data");
+    }
+
+    updateMutation.mutate(updatedData);
+  };
+
   if (!selectedExercise) {
     return (
       <Styles.ContainerSkeleton>
@@ -44,7 +81,16 @@ const ExerciseGraph = () => {
   return (
     <Styles.Container>
       <Styles.Header>
-        <Styles.ExerciseName>{selectedExercise.name}</Styles.ExerciseName>
+        <Input
+          role="editable"
+          value={selectedExercise.name}
+          onBlurEvent={(exerciseName) =>
+            updateHandler({
+              exerciseId: router.query.exerciseId as string,
+              exerciseName,
+            })
+          }
+        />
         <Button
           role="svg"
           svgName="close"

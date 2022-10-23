e3a284396d24435adf891c386430aac2a5ddf6f5
AugustinSorel <sorelaugustin@gmail.com>
2022-10-23 21:40:38 +0100

    toggle pin mutation added

--- src/features/GridOfExercies/GridItem.tsx
+++ src/features/GridOfExercies/GridItem.tsx
@@ -1,17 +1,58 @@
+import Button from "@/components/Button";
 import { ReactNode } from "react";
+import { InferProcedures, trpc } from "src/utils/trpc";
 import * as Styles from "./GridItem.styled";
 
 type Props = {
   children: ReactNode;
   title: string;
   delay: number;
+  togglePinInput?: InferProcedures["exercise"]["togglePin"]["input"];
 };
 
-const GridItem = ({ title, children, delay }: Props) => {
+const GridItem = (props: Props) => {
+  const { title, children, delay, togglePinInput } = props;
+  const utils = trpc.useContext();
+
+  const togglePinMutation = trpc.exercise.togglePin.useMutation({
+    onSettled: () => {
+      utils.exercise.all.invalidate();
+    },
+
+    onMutate: async ({ exerciseId, isPinned }) => {
+      await utils.exercise.all.cancel();
+
+      const allExercises = utils.exercise.all.getData();
+
+      if (!allExercises) {
+        return;
+      }
+
+      utils.exercise.all.setData(
+        allExercises.map((exercise) =>
+          exercise.id === exerciseId ? { ...exercise, isPinned } : exercise
+        )
+      );
+    },
+  });
+
   return (
     <Styles.Container delay={delay} tabIndex={0}>
-      <Styles.Header>
+      <Styles.Header isTaskPinned={togglePinInput?.isPinned}>
         <Styles.ExerciseName>{title}</Styles.ExerciseName>
+        {togglePinInput && (
+          <Button
+            role="svg"
+            svgName="pin"
+            onClick={(e) => {
+              e.preventDefault();
+              togglePinMutation.mutate({
+                exerciseId: togglePinInput.exerciseId,
+                isPinned: !togglePinInput.isPinned,
+              });
+            }}
+          />
+        )}
       </Styles.Header>
 
       {children}

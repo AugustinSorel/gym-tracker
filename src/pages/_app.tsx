d890f2128e177d18a3752d5bbc98a4cc9e9baa70
AugustinSorel <sorelaugustin@gmail.com>
2022-11-10 17:58:40 +0000

    viewport head tag added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { NextPage } from "next";
 import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { ReactElement, ReactNode } from "react";
 import StyledComponentsLayout from "src/layouts/StyledComponentsLayout";
 import { trpc } from "src/utils/trpc";
@@ -23,6 +24,12 @@ const MyApp = ({
   return (
     <SessionProvider session={session}>
       <StyledComponentsLayout>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
         {getLayout(<Component {...pageProps} />)}
       </StyledComponentsLayout>
     </SessionProvider>
